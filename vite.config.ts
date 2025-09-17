41a8f55cc77100cddde22658b7bd9a2158075817
unknown <rvnycs@gmail.com>
2025-09-17 08:57:14 +0700

    add base config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/wuss/", // <-- tambahin ini (nama repo kamu)
   server: {
     host: "::",
     port: 8080,
