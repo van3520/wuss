a7580ad4ae3452ba8e211ea305949464ebce3bef
unknown <rvnycs@gmail.com>
2025-09-17 10:56:34 +0700

    setup gh-pages deploy (base + homepage + scripts)

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/wuss/", // <-- tambahin ini (nama repo kamu)
+  base: "/wuss/",       // <--- penting untuk GitHub Pages
   server: {
     host: "::",
     port: 8080,
